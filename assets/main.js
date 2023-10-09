f1834e6e0466019b771a1a52ac213f9e8ee60094
‎ ‎ <github.1700a@passinbox.com>
2023-10-09 15:14:17 -0400

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,23 @@
 function downloadImage() {
-	html2canvas(document.querySelector("#toulmin-model")).then(canvas => {
-		document.body.appendChild(canvas)
+	// Select the element you want to capture as an image
+	const elementToCapture = document.querySelector("#toulmin-model");
+  
+	// Create a canvas from the element
+	html2canvas(elementToCapture).then(canvas => {
+	  // Create a data URL for the canvas content (PNG format by default)
+	  const dataURL = canvas.toDataURL("image/png");
+  
+	  // Create a temporary anchor element to trigger the download
+	  const downloadLink = document.createElement("a");
+	  downloadLink.href = dataURL;
+	  downloadLink.download = "image.png"; // Set the desired file name
+	  downloadLink.click();
+  
+	  // Clean up by removing the temporary canvas
+	  document.body.removeChild(canvas);
 	});
-}
+  }
+  
 
 document.addEventListener("DOMContentLoaded", function() {
 	// Add an event listener to the "Generate Image" button
